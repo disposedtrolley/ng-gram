e6c889c3feaaebd6f5810d042f77e4f13d800891
jamesliu <jamesliu@au1.ibm.com>
2017-09-26 11:52:33 +1000

    updated URLs to reflect new folder name

--- client/app.config.js
+++ client/app.config.js
@@ -6,30 +6,29 @@ function config($stateProvider, $urlRouterProvider, $authProvider) {
 	$stateProvider
 		.state('home', {
 			url: '/',
-			templateUrl: 'app/routes/home/home.html',
+			templateUrl: 'client/routes/home/home.html',
 			controller: 'HomeController as vm',
 		})
 		.state('login', {
 			url: '/login',
-			templateUrl: 'app/routes/login/login.html',
+			templateUrl: 'client/routes/login/login.html',
 			controller: 'LoginController as vm',
 		})
 		.state('signup', {
 			url: '/signup',
-			templateUrl: 'app/routes/signup/signup.html',
+			templateUrl: 'client/routes/signup/signup.html',
 			controller: 'SignupController as vm',
 		})
 		.state('photo', {
 			url: '/photo/{id}',
-			templateUrl: 'app/routes/photo/photo.html',
+			templateUrl: 'client/routes/photo/photo.html',
 			controller: 'PhotoController as vm'
 		})
 	$urlRouterProvider.otherwise('/')
 
-	$authProvider
-		.loginUrl = 'http://localhost:3000/auth/login'
-		.signupUrl = 'http://localhost:3000/auth/signup'
-		.oauth2({
+	$authProvider.loginUrl = 'http://localhost:3000/auth/login'
+	$authProvider.signupUrl = 'http://localhost:3000/auth/signup'
+	$authProvider.oauth2({
 			name: 'instagram',
 			url: 'http://localhost:3000/auth/instagram',
 			redirectUri: 'http://localhost:8000',
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.static(__dirname + '/app'))
+app.use(express.static(__dirname + '/client'))
 app.use(express.static(__dirname))
 
 app.listen(8000)
