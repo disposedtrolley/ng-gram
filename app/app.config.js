21437f857bea620b47e069efc3bb119b4a29c573
jamesliu <jamesliu@au1.ibm.com>
2017-09-26 11:25:30 +1000

    set up routing with ui-router

--- app/app.config.js
+++ app/app.config.js
@@ -2,6 +2,27 @@ angular
 	.module('app')
 	.config(config)
 
-function config($stateProvider) {
-	
+function config($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'app/routes/home/home.html',
+			controller: 'HomeController as vm',
+		})
+		.state('login', {
+			url: '/login',
+			templateUrl: 'app/routes/login/login.html',
+			controller: 'LoginController as vm',
+		})
+		.state('signup', {
+			url: '/signup',
+			templateUrl: 'app/routes/signup/signup.html',
+			controller: 'SignupController as vm',
+		})
+		.state('photo', {
+			url: '/photo/{id}',
+			templateUrl: 'app/routes/photo/photo.html',
+			controller: 'PhotoController as vm'
+		})
+	$urlRouterProvider.otherwise('/')
 }
