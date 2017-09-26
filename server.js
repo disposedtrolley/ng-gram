04895118021933962329662b93f73fcf79f72d37
jamesliu <jamesliu@au1.ibm.com>
2017-09-26 10:32:32 +1000

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(__dirname + '/app'))
+app.use(express.static(__dirname))
+
+app.listen(8000)
+
+console.log('Server running on http://localhost:8000')
