6565bd9fecbd1f31e140d37411824c2eb458fa7f
jamesliu <jamesliu@au1.ibm.com>
2017-09-26 15:49:11 +1000

    add calls to the API service to retrieve users’ images

--- client/routes/home/home.controller.js
+++ client/routes/home/home.controller.js
@@ -5,13 +5,23 @@
 		.module('app')
 		.controller('HomeController', HomeController)
 
-	HomeController.$inject = ['$window', '$rootScope', '$auth']
+	HomeController.$inject = ['$window', '$rootScope', '$auth', 'API']
 	function HomeController($window, $rootScope, $auth) {
 		let vm = this
 
 		vm.isAuthenticated = isAuthenticated
 		vm.linkInstagram = linkInstagram
+		vm.photos
 
+		activate()
+
+		function activate() {
+			if ($auth.isAuthenticated() && ($rootScope.currentUser && $rootScope.currentUser.username)) {
+				API.getFeed().success(function(data) {
+					vm.photos = data;
+				});
+			}
+		}
 
 		function isAuthenticated() {
 			return $auth.isAuthenticated()
@@ -22,6 +32,9 @@
 				.then(function(res) {
 					$window.localStorage.currentUser = JSON.stringify(res.data.user)
 					$rootScope.currentUser = JSON.parse($window.localStorage.currentUser)
+					API.getFeed().success(function(data) {
+						vm.photos = data;
+					});
 				})
 		}
 	}
